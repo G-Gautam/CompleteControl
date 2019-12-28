b2a2feb3d0f5fa8cd5fb0b0e8f722d799eb96f02
John Doe <gautamgupta9298@gmail.com>
2019-12-27 23:30:24 -0500

    Wrist flick to activate completed'

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.util.Log
+import android.view.KeyEvent
 import android.view.Menu
 import android.widget.Button
 import android.widget.TextView
@@ -25,9 +26,27 @@ class MainActivity : WearableActivity() {
 
         startButton.setOnClickListener {
             Log.v("Status", "Button click is working")
-            sensorStore.startGyroReading()
         }
         setAmbientEnabled()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        Log.v("Status", keyCode.toString())
+        return when (keyCode) {
+            KeyEvent.KEYCODE_NAVIGATE_NEXT ->
+                // Flick wrist out
+                triggerFlick()
+            else -> {
+                Log.v("Status", "Flick not detected")
+                super.onKeyDown(keyCode, event)
+            }
+        }
+    }
+
+    private fun triggerFlick() : Boolean{
+        Log.v("Status", "Flick detected")
+        sensorStore.wristFlickDetected()
+        return false
     }
 
     override fun onPause(){
--- MyApplication/app/src/main/java/com/example/myapplication/SensorStore.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SensorStore.kt
@@ -5,11 +5,15 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.CountDownTimer
+import android.os.Handler
 import android.support.wearable.activity.WearableActivity
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
+import java.util.*
 
 public class SensorStore : SensorEventListener {
     private lateinit var mSensorManager: SensorManager
@@ -21,20 +25,30 @@ public class SensorStore : SensorEventListener {
     constructor(context: Context){
         mSensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         con = context
+        val deviceSensors: List<Sensor> = mSensorManager.getSensorList(Sensor.TYPE_ALL)
+        Log.v("Total sensors",""+deviceSensors.size)
+        deviceSensors.forEach{
+            Log.v("Sensor name",""+it)
+        }
     }
 
-    fun startGyroReading(){
-        var gyroSensor : Sensor? = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-        if(gyroSensor != null){
-            mSensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL)
-            Log.v("Sensor", "Sensor Found and is not null")
-        }else{
-            Log.v("Sensor", "Sensor Not Found")
-        }
+    fun wristFlickDetected() {
+        //Toast.makeText(con, "Starting", Toast.LENGTH_SHORT).show()
+        var linearSensor : Sensor? = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        mSensorManager.registerListener(this, linearSensor, SensorManager.SENSOR_DELAY_GAME)
+        Log.v("Status", "Registered Sensor and is actively listening")
+        startTimerToUnRegister()
+    }
+
+    private fun startTimerToUnRegister(){
+        Handler().postDelayed({
+            Log.v("Status", "Calling Destroy")
+            destroy()
+        }, 5000)
     }
     override fun onSensorChanged(p0: SensorEvent?) {
         if (p0 != null) {
-            if (p0.sensor.type == Sensor.TYPE_ROTATION_VECTOR)
+            if (p0.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION)
                 xReadingText.text = "X-Reading" + p0.values[0].toString().substring(0,5)
                 yReadingText.text = "Y-Reading" + p0.values[1].toString().substring(0,5)
                 zReadingText.text = "Z-Reading" + p0.values[2].toString().substring(0,5)
@@ -46,6 +60,7 @@ public class SensorStore : SensorEventListener {
 
     fun destroy() {
 //        unregister the sensor onPause else it will be active even if the activity is closed
+        Log.v("Status", "Destroyed all listeners to sensor")
         mSensorManager.unregisterListener(this)
     }
 }
